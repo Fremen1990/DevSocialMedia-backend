24ec32a0a4017d4a83659d3e4f7dd133e14461f7
DevThomas <fremen1990@gmail.com>
2022-07-01 14:54:34 +0200

    Add register validation

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/User')
+const { validateEmail, validateLength } = require('../helpers/validation')
+const bcrypt = require('bcrypt')
 
 exports.register = async (req, res) => {
     try {
@@ -14,12 +16,46 @@ exports.register = async (req, res) => {
             gender,
         } = req.body
 
+        // Regex validation for email
+        if (!validateEmail(email)) {
+            return res.status(400).json({ message: 'Invalid email address' })
+        }
+
+        // Check if email is alrady in database
+        const check = await User.findOne({ email })
+        if (check) {
+            return res.status(400).json({
+                message:
+                    'This email address already exists, please use different email',
+            })
+        }
+
+        // Length validations
+        if (!validateLength(first_name, 2, 30)) {
+            return res.status(400).json({
+                message: 'first_name must be between 2 and 30 characters',
+            })
+        }
+        if (!validateLength(last_name, 3, 30)) {
+            return res.status(400).json({
+                message: 'last_name must be between 3 and 30 characters',
+            })
+        }
+        if (!validateLength(password, 6, 64)) {
+            return res.status(400).json({
+                message: 'password must be between 6 and 64 characters',
+            })
+        }
+
+        const cryptedPassword = await bcrypt.hash(password, 12)
+        console.log(cryptedPassword)
+
         const user = await new User({
             first_name,
             last_name,
             email,
             username,
-            password,
+            cryptedPassword,
             bYear,
             bMonth,
             bDay,

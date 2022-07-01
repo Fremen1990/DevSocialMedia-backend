a8652e23ebcdfab7223144677bc18f17ce8e06fa
DevThomas <fremen1990@gmail.com>
2022-07-01 20:24:52 +0200

    Login backend implemented

--- controllers/user.js
+++ controllers/user.js
@@ -97,17 +97,56 @@ exports.register = async (req, res) => {
 }
 
 exports.activateAccount = async (req, res) => {
-    const { token } = req.body
-    const user = jwt.verify(token, process.env.TOKEN_SECRET)
-    const check = await User.findById(user.id)
-    if (check.verified === true) {
-        return res
-            .status(400)
-            .json({ message: 'This user is already activated' })
-    } else {
-        await User.findByIdAndUpdate(user.id, { verified: true })
-        return res
-            .status(200)
-            .json({ message: 'Account has been activated successfully' })
+    try {
+        const { token } = req.body
+        const user = jwt.verify(token, process.env.TOKEN_SECRET)
+        const check = await User.findById(user.id)
+        if (check.verified === true) {
+            return res
+                .status(400)
+                .json({ message: 'This user is already activated' })
+        } else {
+            await User.findByIdAndUpdate(user.id, { verified: true })
+            return res
+                .status(200)
+                .json({ message: 'Account has been activated successfully' })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const user = await User.findOne({ email })
+        if (!user) {
+            return res.status(400).json({
+                message:
+                    'The email address you entered is not connected to an account',
+            })
+        }
+        // Checking encrypted password from database with password from req.body
+        const check = await bcrypt.compare(password, user.password)
+        if (!check) {
+            return res.status(400).json({
+                message: 'Incorrect password',
+            })
+        }
+
+        // If email and password is OK generate login token and pass user data
+        const token = generateToken({ id: user._id.toString() }, '7d')
+        res.send({
+            id: user._id,
+            username: user.newUsername,
+            picture: user.picture,
+            first_name: user.first_name,
+            last_name: user.last_name,
+            token: token,
+            verified: user.verified,
+            message: 'Register Success! Please activate your email',
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message })
     }
 }

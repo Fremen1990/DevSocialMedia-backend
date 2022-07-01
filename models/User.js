8ffe03fe13a081b2f97eb3aaca3732a5506032b9
DevThomas <fremen1990@gmail.com>
2022-07-01 13:58:50 +0200

    User Model complete, prettier config, ReadME update

--- models/User.js
+++ models/User.js
@@ -1,64 +1,137 @@
 const mongoose = require('mongoose')
 
-const userSchema = new mongoose.Schema({
-    first_name:{
-        type: String,
-        required: [true, "first name is required"],
-        trim: true,
-        text: true
-    },
-    last_name:{
-        type: String,
-        trim: true,
-        text: true
-    },
-    username:{
-        type: String,
-        required: [true, "username name is required"],
-        trim: true,
-        text: true,
-        unique: true
-    },
-    email:{
-        type: String,
-        required: [true, "email name is required"],
-        trim: true,
-        text: true
-    },
-    password:{
-        type: String,
-        required: [true, "password name is required"],
-    },
-    picture:{
-        type: String,
-        // default: here paste defaul link to "noPicture" image from cloud
-    },
-    cover:{
-        type: String,
-    },
-    gender:{
-        type: String,
-        required: [true, "gender name is required"],
-        trim: true
-    },
-    bYear:{
-     type:Number,
-     required:true,
-     trim:true
-    },
-    bMonth:{
-        type:Number,
-        required:true,
-        trim:true
-    },
-    bDay:{
-        type:Number,
-        required:true,
-        trim:true
-    }
+const { ObjectId } = mongoose.Schema
 
-//TODO Let's setup the User Model 05:00
-})
+const userSchema = new mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            required: [true, 'first name is required'],
+            trim: true,
+            text: true,
+        },
+        last_name: {
+            type: String,
+            trim: true,
+            text: true,
+        },
+        username: {
+            type: String,
+            required: [true, 'username name is required'],
+            trim: true,
+            text: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: [true, 'email name is required'],
+            trim: true,
+            text: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'password name is required'],
+        },
+        picture: {
+            type: String,
+            // default: here paste default link to "noPicture" image from cloud
+        },
+        cover: {
+            type: String,
+        },
+        gender: {
+            type: String,
+            required: [true, 'gender name is required'],
+            trim: true,
+        },
+        bYear: {
+            type: Number,
+            required: true,
+            trim: true,
+        },
+        bMonth: {
+            type: Number,
+            required: true,
+            trim: true,
+        },
+        verified: {
+            type: Boolean,
+            default: false,
+        },
+        friends: {
+            type: Array,
+            default: [],
+        },
+        following: {
+            type: Array,
+            default: [],
+        },
+        followers: {
+            type: Array,
+            default: [],
+        },
+        requests: {
+            type: Array,
+            default: [],
+        }, // user search history
+        search: {
+            user: {
+                type: mongoose.Schema.ObjectId,
+                ref: 'User',
+            },
+        },
+        details: {
+            bio: {
+                type: String,
+            },
+            otherName: {
+                type: String,
+            },
+            job: {
+                type: String,
+            },
+            workplace: {
+                type: String,
+            },
+            highSchool: {
+                type: String,
+            },
+            college: {
+                type: String,
+            },
+            currentCity: {
+                type: String,
+            },
+            hometown: {
+                type: String,
+            },
+            relationship: {
+                type: String,
+                enum: [
+                    'Single',
+                    'In a relationship',
+                    'Married',
+                    'Divorced',
+                    'Programmer',
+                ],
+            },
+            instagram: {
+                type: String,
+            },
+        },
+        savedPosts: [
+            {
+                post: {
+                    type: ObjectId,
+                    ref: 'Post',
+                },
+                savedAt: {
+                    default: new Date(),
+                },
+            },
+        ],
+    },
+    { timestamps: true }
+)
 
-
-const User = mongoose.model("User", userSchema)
+module.exports = mongoose.model('User', userSchema)

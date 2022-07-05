3e03a98f1777151b101f3202a13b47a3960877ae
DevThomas <fremen1990@gmail.com>
2022-07-05 18:42:04 +0200

    Deployment settings

---
+++ config/config.example.js
@@ -0,0 +1,8 @@
+export const config = {
+    //   dbHost: "localhost",
+    //   dbUser: "user",
+    //   dbPassword: "password",
+    //   DbDatabase: "database",
+    corsOrigin: 'http://localhost:3001',
+    //
+}
--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const app = express()
 require('dotenv').config()
 const { readdirSync } = require('fs')
 const mongoose = require('mongoose')
+const { config } = require('./config/config.example')
 
 app.use(express.json())
 
 // CORS for allowed clients
-app.use(cors())
-// app.use(cors({origin:"http://localhost:3000"}))
+// app.use(cors())
+app.use(cors({ origin: config.corsOrigin }))
 
 //ROUTES
 // mapping through folder routes and adding routes as middleware
@@ -25,7 +26,7 @@ mongoose
     .then(() => console.log('Database connected successfully'))
     .catch((err) => console.log('Error connecting to mongoDB', err))
 
-const port = process.env.PORT || 8000
-app.listen(port, () => {
+const port = process.env.PORT || 8800
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is listening on port: ${port}...`)
 })

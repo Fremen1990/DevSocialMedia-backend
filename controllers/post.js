092d5be0c476f6102a2b137a776a5291426e302c
DevThomas <fremen1990@gmail.com>
2022-07-12 00:18:28 +0200

    Comments functionality completed from backend and frontend.
    Signed-off-by: DevThomas <fremen1990@gmail.com>

--- controllers/post.js
+++ controllers/post.js
@@ -21,3 +21,31 @@ exports.getAllPosts = async (req, res) => {
         res.status(500).json({ message: error.message })
     }
 }
+
+exports.comment = async (req, res) => {
+    try {
+        const { comment, image, postId } = req.body
+        let newComments = await Post.findByIdAndUpdate(
+            postId,
+            {
+                $push: {
+                    comments: {
+                        comment: comment,
+                        image: image,
+                        commentBy: req.user.id,
+                        commentAt: new Date(),
+                    },
+                },
+            },
+            {
+                new: true,
+            }
+        ).populate(
+            'comments.commentBy',
+            'picture first_name last_name username'
+        )
+        res.json(newComments.comments)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}

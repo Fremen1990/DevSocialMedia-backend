4922ca0f817c86a9ff49d071b4e298d99958e8c8
DevThomas <fremen1990@gmail.com>
2022-07-15 13:11:07 +0200

    Migration from "dotenv" to "config" with types
    Signed-off-by: DevThomas <fremen1990@gmail.com>

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
 import fileUpload from 'express-fileupload'
 import { stringify } from 'querystring'
 import connectDB from './utils/connectDB'
